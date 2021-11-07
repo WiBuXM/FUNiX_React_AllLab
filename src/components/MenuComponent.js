21cce44deded8addc4b5b7812f86c525342340d9
caychuoixm <caychuoixm1102@gmail.com>
2021-11-07 23:02:50 +0700

    Lab3_1

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,78 @@
+// Tạo react component
+import React, {Component} from "react";
+import {Media} from "reactstrap";
+class Menu extends Component{
+    // định nghĩa constructor nhận tham số gọi là props.
+    constructor(props){
+        //cung cấp cho props super class ( bắt buộc bất cứ khi nào xác định 1 class component.)
+        super(props);
+        // tác dụng lưu trữ các propety liên quan đến component này và có thể sử dụng.
+                this.state = {
+            dishes: [
+                {
+                  id: 0,
+                  name:'Uthappizza',
+                  image: 'assets/images/uthappizza.png',
+                  category: 'mains',
+                  label:'Hot',
+                  price:'4.99',
+                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
+               {
+                  id: 1,
+                  name:'Zucchipakoda',
+                  image: 'assets/images/zucchipakoda.png',
+                  category: 'appetizer',
+                  label:'',
+                  price:'1.99',
+                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
+               {
+                  id: 2,
+                  name:'Vadonut',
+                  image: 'assets/images/vadonut.png',
+                  category: 'appetizer',
+                  label:'New',
+                  price:'1.99',
+                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
+               {
+                  id: 3,
+                  name:'ElaiCheese Cake',
+                  image: 'assets/images/elaicheesecake.png',
+                  category: 'dessert',
+                  label:'',
+                  price:'2.99',
+                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
+               ],
+        };
+    }
+
+    //chuyển chế độ xem tương ứng cho thành phần này
+    render() {
+        const menu = this.state.dishes.map((dish) => {
+            return (
+              <div key={dish.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={dish.image} alt={dish.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{dish.name}</Media>
+                    <p>{dish.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+              <Media list>
+                  {menu}
+              </Media>
+            </div>
+          </div>
+        );
+    }
+}
+// Xuất component đó từ file này 
+export default Menu;
